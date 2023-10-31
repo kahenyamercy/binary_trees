1d86d7616b858242bf95cd82aedb8d85aa8c5778
kahenyamercy <kahenyamercy5@gmail.com>
2023-10-31 11:10:25 +0000

    measure balance factor of a binary tree

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Balance factor of the binary tree, 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
